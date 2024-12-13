16d3f2520da569b4daf899f4f80cf282fd85d636
3-Partner <ourgithubteam@gmail.com>
2024-12-13 12:03:08 -0800

    Add files via upload

--- Qomrah/js/editprofile.js
+++ Qomrah/js/editprofile.js
@@ -17,3 +17,27 @@ function previewImage(event) {
         reader.readAsDataURL(file);
     }
 }
+
+
+
+
+
+
+
+
+
+
+  // Get references to the button and the update profile box
+  const button = document.getElementById('show-button');
+  const updateProfileBox = document.getElementById('update-profile-box');
+
+  // Add a click event listener to the button
+  button.addEventListener('click', function() {
+    // Show the update profile box
+    updateProfileBox.style.display = 'block';
+    
+    // Hide the update profile box after 3 seconds
+    setTimeout(function() {
+      updateProfileBox.style.display = 'none';
+    }, 3000);
+  });
